6507a2da842511a021db24b474e5579001592774
srinivasan000 <srinivasans2425s@gmail.com>
2024-04-24 15:17:54 +0530

    cdn update

--- update.php
+++ update.php
@@ -63,7 +63,7 @@ if (isset($_POST["submit"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../bootstrap.min.css">
+    <link rel="stylesheet" href="./bootstrap.min.css">
 </head>
 
 <body>
